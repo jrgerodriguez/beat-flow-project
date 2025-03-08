514292a57cb8c448891eec83b8220e38840f6c16
jrgerodriguez <menjivar.jorge.93@gmail.com>
2025-03-07 19:31:15 -0600

    Register view built

--- controllers/cuentaController.js
+++ controllers/cuentaController.js
@@ -1,6 +1,11 @@
-
+//Esta funcion renderiza el view para hacer login
 async function buildLoginView(req, res) {
     res.render("./cuenta/login", {titulo: 'Iniciar Sesión'})
 }
 
-module.exports = {buildLoginView}
+//Esta funcion renderiza el view para crear una nueva cuenta
+async function buildRegisterView(req, res) {
+    res.render("./cuenta/registrar", {titulo: 'Registro'})
+}
+
+module.exports = {buildLoginView, buildRegisterView}
--- routes/account-route.js
+++ routes/account-route.js
@@ -6,4 +6,7 @@ const utilidades = require("../utilities/")
 // Ruta para iniciar sesion
 router.get("/login", utilidades.handleErrors(cuentaController.buildLoginView))
 
+// Ruta para Registra una nueva cuenta
+router.get("/registrar", utilidades.handleErrors(cuentaController.buildRegisterView))
+
 module.exports = router
